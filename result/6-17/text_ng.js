9294735a8d7fe0e29db52285e014003818ff2a99
LAPTOP-OJ0PQ7DN\Masahiro Yoshimura <masahiro.440ra@gmail.com>
2016-12-16 10:50:26 +0900

    20162016/12/16 DOM属性値やテキストを取得、設定する

---
+++ result/6-17/text_ng.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+	
+	document.getElementById('btn').addEventListener('click', function () {
+		var name = document.getElementById('name');
+		var result = document.getElementById('result');
+//		result.innerHTML = 'こんにちは、' + name.value + 'さん';
+		result.textContent = 'こんにちは、' + name.value + 'さん';
+	}, false);
+}, false);
